1b03b8b9889fd5965d5b81b566973de1821906e1
Sebastian Garcia <juansgperez@gmail.com>
2023-02-08 19:52:17 -0500

    Added and solved exercise 4

---
+++ ejercicios/ejercicio_4.py
@@ -0,0 +1,7 @@
+number = float(input("Please write the number: "))
+
+determinant_operation = number%2
+if determinant_operation == 0:
+    print("The number is even!")
+else:
+    print("The number is not even")
