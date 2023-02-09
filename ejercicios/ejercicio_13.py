6f510a68ba81b7bdf076d9e9d494819d41864ec3
Sebastian Garcia <juansgperez@gmail.com>
2023-02-08 22:42:21 -0500

    Added and solved exercise 13

---
+++ ejercicios/ejercicio_13.py
@@ -0,0 +1,7 @@
+number_1 = float(input("Please enter the first number: "))
+number_2 = float(input("Please enter the second number: "))
+
+print(f"The result of the sum is: {number_1 + number_2}; "
+      f"The result of the subtraction is: {number_1 - number_2}; "
+      f"The result of the multiplication is: {number_1 * number_2}; "
+      f"The result of the division is: {number_1 / number_2}; ")
